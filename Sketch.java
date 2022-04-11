c9709621727f19baf236aa0d126c10a5cba99cdc
Aaron-Chung23 <99738173+Aaron-Chung23@users.noreply.github.com>
2022-04-10 21:14:54 -0400

    Add files via upload

--- Sketch.java
+++ Sketch.java
@@ -24,13 +24,59 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+	drawFirstFace(300,300);
+  drawSecondFace(250, 60, 255, 192, 203, 0, 255);
+  getParameters(width/2, height/2); 
+    
   }
+
+  private void drawFirstFace(int x, int y){
+
+  //face
+  fill(255,255,255);
+  ellipse(x, y, width/6, height/6); 
+
+  //Left hole
+  fill(0,0,0);
+  ellipse(x-10,y-15, width/40, height/40);
+
+  //Right hole
+  fill(0,0,0);
+  ellipse(x+10, y-15, width/40, height/40); 
   
+  fill(255,192,203);
+  ellipse(x,y+10,width/40, height/60);
+
+  }
+
+  private void drawSecondFace(int x2, int y2, int faceColour, int faceColour2, int faceColour3, int eye, int eye2){
+  //face
+  fill(faceColour2,faceColour3,faceColour);
+  ellipse(x2, y2, width/5, height/5); 
+  
+  //Left hole
+  fill(eye,eye2,eye);
+  ellipse(x2-20,y2-20, width/40, height/40);
+  
+  //Right hole
+  fill(eye,eye2,eye);
+  ellipse(x2+20, y2-20, width/40, height/40); 
+  
+  stroke(eye,eye,eye);
+  line(x2-15, y2+10, x2+5, y2+20);
+
+  stroke(eye,eye,eye);
+  line(x2+5, y2+20, x2+25, y2+10);
+  
+  }
+
+  private int getParameters(int placeX, int placeY){  
+  
+    fill(0,0,0);
+    ellipse(placeX, placeY, 100, 100);
+
+    return placeX + placeY;
+
+  }
   // define other methods down here.
 }
